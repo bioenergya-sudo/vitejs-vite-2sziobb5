7c4034c2635497eb9c46864a5b12fff5355c96a1
bioenergya-sudo <bioenergya@gmail.com>
2026-05-27 13:11:07 +0300

    React Router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.jsx'
 import LandingPage from './pages/LandingPage.jsx'
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/montaj-osvetlenie" element={<LandingPage service="osvetlenie" />} />
         <Route path="/sglobyavane-mebeli" element={<LandingPage service="mebeli" />} />
         <Route path="/drebni-remonti" element={<LandingPage service="remonti" />} />
+        <Route path="/za-nas" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
