ef1821284c7d3bfa91b20009e3f3d089c64a0b57
bioenergya-sudo <bioenergya@gmail.com>
2026-05-27 18:39:21 +0300

    new route at main.jsk

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import LandingPage from './pages/LandingPage.jsx'
+import ZaNas from './pages/ZaNas.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/montaj-osvetlenie" element={<LandingPage service="osvetlenie" />} />
         <Route path="/sglobyavane-mebeli" element={<LandingPage service="mebeli" />} />
         <Route path="/drebni-remonti" element={<LandingPage service="remonti" />} />
-        <Route path="/za-nas" element={<App />} />
+        <Route path="/za-nas" element={<ZaNas />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
