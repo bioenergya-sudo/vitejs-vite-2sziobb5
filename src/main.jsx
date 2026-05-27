984a575d7760cd38b89be73091fd63b1ead4b5ab
bioenergya-sudo <bioenergya@gmail.com>
2026-05-27 17:10:48 +0300

    React Router v7

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import App from './App.jsx'
 import LandingPage from './pages/LandingPage.jsx'
 
